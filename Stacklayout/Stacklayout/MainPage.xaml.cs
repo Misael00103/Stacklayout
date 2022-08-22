0bd4e5b4e0b7ae4f98d8561d7bf120166bb25dfe
Misael00103 <beriguetemisael@gmail.com>
2022-08-21 21:44:03 -0400

    first commit

---
+++ Stacklayout/Stacklayout/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+
+namespace Stacklayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeerMas_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Guido_van_Rossum", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas1_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Brendan_Eich", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas2_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Rasmus_Lerdorf", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas3_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Matt_Mullenweg", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void btnLeerMas4_Clicked(object sender, EventArgs e)
+        {
+            Browser.OpenAsync("https://es.wikipedia.org/wiki/Tim_Berners-Lee", BrowserLaunchMode.SystemPreferred);
+        }
+    }
+}
+
+
+
+
